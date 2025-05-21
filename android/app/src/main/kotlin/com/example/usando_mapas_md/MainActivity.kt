3dd01685228d29407868129b225ab4f006518af2
giovani <inf_giovani@hotmail.com>
2025-05-21 19:41:49 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/usando_mapas_md/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.usando_mapas_md
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget{
+
+  _HomePageState createState() => _HomePageState();
+}
