1ec7a33e9613d8093b880ae118170af93f16e0d9
giovani <inf_giovani@hotmail.com>
2025-05-22 19:35:22 -0300

    Atualização da camera no mapa

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -1,6 +1,10 @@
 
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapasPage extends StatefulWidget{
   final double latitude;
@@ -18,11 +22,38 @@ class MapasPage extends StatefulWidget{
 
 class _MapasPageState extends State<MapasPage>{
 
+  final _controller = Completer<GoogleMapController>();
+  StreamSubscription<Position>? _subscription;
+
+  @override
+  void initSate(){
+    super.initState();
+    _monitorarLocalizacao();
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: const Text('Mapa Interno')),
       body: Container()
     );
+  }
+
+  void _monitorarLocalizacao(){
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100
+    );
+
+    _subscription = Geolocator.getPositionStream(
+        locationSettings: locationSettings).listen((Position position) async {
+          final controller = await _controller.future;
+          final zoom = await controller.getZoomLevel();
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                  target: LatLng(position.latitude, position.longitude),
+                zoom: zoom
+              )));
+    });
   }
 }
